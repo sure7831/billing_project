e8caddeceb3529d3881ea3bf1e6e35be0572bf17
sure7831 <siddharth.ponugumati.26@gmail.com>
2025-05-07 10:38:02 +0530

    Add files via upload

---
+++ billing_project.py
@@ -0,0 +1,48 @@
+item_1=500
+
+print("welcome_to_Billing_project")
+
+
+print("item_1-price-500-----item_2-price-2000")
+
+i=int(input("enter item number :"))
+
+if i<=0:
+        print("       ERROR       ")
+        print("ITEM DOES NOT EXIST")
+
+elif i>=3:
+
+        print("       ERROR       ")
+        print("ITEM DOES NOT EXIST")
+
+i2=int(input("enter quantity"))
+
+cost=i*i2s
+
+   
+
+
+if 2 <= i2 <= 5:
+    discount = 4 / 100
+elif 1 <= i2 <= 2:
+    discount = 10 / 100
+elif 5 <= i2 <= 8:
+    discount = 19 / 100
+elif 11 <= i2 <= 40:
+    discount = 24 / 100
+elif i2 >= 12:
+    discount = 29 / 100
+else:
+    discount =  2 / 100
+
+SGST = 18 / 100
+CGST = 18 / 100
+product_SGST = cost * SGST
+product_CGST = cost * CGST
+product_discount =cost * discount
+
+print("         purchase again        ")
+print("  total items purchased cost")
+        
+
